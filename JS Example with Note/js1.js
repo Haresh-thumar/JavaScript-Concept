eaa5864458343e1055aeb6c9e89fa44e5cc92be7
Haresh-thumar <hareshthumar46@gmail.com>
2022-12-20 11:21:13 +0530

    add example with ppt

---
+++ JS Example with Note/js1.js
@@ -0,0 +1,23 @@
+// variable
+// var nameCase= 5 //camel case - for variable & function use
+// var NameCase= 6 // pascal case - for class use
+// var name_Case= 7 // snake case - for pythone developer use
+
+// a
+// function hello(a,b)
+// {
+    
+//     document.write(a)
+// }
+
+
+
+//  hello(1,2);
+
+
+  
+
+    
+
+
+
